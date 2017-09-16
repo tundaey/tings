7ada8024cc15f9d43ff1f92bcf32270dd6f5c5e2
Tunde <tundaey@gmail.com>
2017-09-16 10:47:53 +0100

    first push

---
+++ test/test.js
@@ -0,0 +1,18 @@
+const supertest = require('supertest');
+const should = require('should');
+
+const server = supertest.agent("http://localhost:4000")
+
+//Begin unit test
+
+describe("SAMPLE uit test", function(){
+    it("should return home page", function(done){
+        server.get("/")
+        .expect("Content-type", /text/)
+        .expect(200)
+        .end(function(err, res){
+            res.status.should.equal(200);
+            done();
+        })
+    })
+})
